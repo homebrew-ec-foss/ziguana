936c8e79d5954801383262e45ac085bf4a2c3df6
Arnav Parashar <arnavparashar408@gmail.com>
2026-08-07 13:35:36 +0530

    Revert "fix: checker errors not being printed for emitc or output without flags check flag"
    This reverts commit 03bdf57830297b8f572e7a4a309e960711d70543.

--- src/main.zig
+++ src/main.zig
@@ -55,11 +55,13 @@ pub fn main(init: std.process.Init) !void {
     try checker.check(program);
 
     if (checker.errors.items.len > 0) {
-        for (checker.errors.items) |err| {
-            std.debug.print("error: {s}\n", .{err.message});
+        if (args.print_checks) {
+            for (checker.errors.items) |err| {
+                std.debug.print("error: {s}\n", .{err.message});
+            }
         }
         std.process.exit(1);
-    } else if (checker.errors.items.len > 0 and args.print_checks) {
+    } else if (args.print_checks) {
         std.debug.print("No Errors\n", .{});
     }
 
