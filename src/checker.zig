47d554648ef03906877e17c58a1dda7379f0f584
Arnav Parashar <arnavparashar408@gmail.com>
2026-08-08 12:06:03 +0530

    fix: fixed checker errors and type inference related error

--- src/checker.zig
+++ src/checker.zig
@@ -138,19 +138,6 @@ pub const Checker = struct {
                 if (v.init) |init_val| {
                     switch (init_val) {
                         .expr => |ex| {
-                            if (ex.* == .interpolated_string) {
-                                const is = ex.interpolated_string;
-                                var has_expr = false;
-                                for (is.parts) |part| {
-                                    if (part == .expr) {
-                                        has_expr = true;
-                                        break;
-                                    }
-                                }
-                                if (has_expr) {
-                                    try self.addError(v.line, v.column, "interpolated string with expressions cannot be assigned to '{s}'", .{v.name});
-                                }
-                            }
                             const ety = try self.checkExpr(ex);
                             if (v.ty == .Auto) {
                                 stmt.var_decl.ty = ety;
